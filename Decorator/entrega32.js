0577acd49784bca67e4f236dfad5c1cddded2cb7
eliashz <eliashz@gmail.com>
2022-05-17 11:50:33 +0200

    apuntes

--- Decorator/entrega32.js
+++ Decorator/entrega32.js
@@ -1,28 +1,10 @@
 //Nivell 2: Decorator
 
-const data = require ('./currency_conversions.json')
-
-console.log(data.USD_EUR);
-
-module.exports = Decorator;
-
-let Multiply = (...args) => {
-  console.log(...args);
-  return args.reduce((a, b) => a * b)
-}
-
-// validated integers
-
-const Validator = (fn) => {
-  return function(...args) {
-    const validArgs = args.every(arg => Number.isInteger(arg));
-    if (!validArgs) {
-      throw new TypeError('Argument cannot be a non-integer');
-    }
-    return fn(...args);
+@log()
+@immutable()
+class Example {
+  @time('demo')
+  doSomething() {
+    //
   }
 }
-
-//decorated multiply function that only multiplies integers
-MultiplyValidArgs = Validator(Multiply);
-MultiplyValidArgs(6, 8, 2, 10);
