19000e05221b6d38653e694c99e801341d1b0e4e
eliashz <eliashz@gmail.com>
2022-05-21 11:18:39 +0200

    callbackhell v2

--- CallbackHell/1_entrega31.js
+++ CallbackHell/1_entrega31.js
@@ -4,65 +4,48 @@ const {
     readdir,
     readFile,
     writeFile
-} = require("fs");
-const {
+  } = require("fs");
+  const {
     join
-} = require("path");
-const inbox = join(__dirname, "inbox");
-const outbox = join(__dirname, "outbox");
-
-const reverseText = str =>
+  } = require("path");
+  const inbox = join(__dirname, "inbox");
+  const outbox = join(__dirname, "outbox");
+  
+  const reverseText = str =>
     str
     .split("")
     .reverse()
     .join("");
-
-const leerDirectorio = (inbox) => {
-    return new Promise((resolve, reject) => {
-        readdir(inbox, (error, files) => {
-            if (error) {
-                reject("Error: Folder inaccessible");
-            } 
-            resolve(files)    
-        });
-    });
-};
-
-const leerArchivo = (files, inbox) => {
-    return new Promise ((resolve, reject) => {   
-        files.forEach(file => {
-            readFile(join(inbox, file), "utf8", (error, data) => {
-                if (error) {
-                    reject("Error: File error")
-                }  
-                resolve ([data, file]);  
-            });   
-        });           
-    })
-};    
-
-const escribirArchivo = (data, file) => {
-    return new Promise ((resolve, reject) => {
+  
+  // Read and reverse contents of text files in a directory
+  
+/*    readdir(inbox, (error, files) => {
+    if (error) return console.log("Error: Folder inaccessible");
+    console.log(files)
+    files.forEach(file => {
+      readFile(join(inbox, file), "utf8", (error, data) => {
+        if (error) return console.log("Error: File error");
         writeFile(join(outbox, file), reverseText(data), error => {
-            if (error) {
-                reject("Error: File could not be saved!");
-            }
-            resolve(`${file} was successfully saved in the outbox!`);
-          });
+          if (error) return console.log("Error: File could not be saved!");
+          console.log(`${file} was successfully saved in the outbox!`);
+        });
+      });
     });
-};
+  });  */
 
-const archivo = async (inbox) => {
-    try {
-        const files = await leerDirectorio(inbox);
-        const dataFile = await leerArchivo(files, inbox);  
-        return await escribirArchivo(dataFile[0], dataFile[1]);
-        
-    } catch (error) {
-        throw error;
-    }
+
+  
+const leerArchivo = () =>{
+    readdir(inbox, (error, files) => {
+    if (error) {
+        console.log("Error: Folder inaccessible");
+    } else {
+        files.forEach(file => {
+            //escribirArchivo();
+            console.log(file);
+        });
+    }   
+    });
 }
-      
-archivo (inbox)
-    .then(mensaje => console.log(mensaje))
-    .catch(err => console.log(err))
+
+leerArchivo();
