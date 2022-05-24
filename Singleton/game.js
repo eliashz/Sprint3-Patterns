910406a91e79a7c9496b7501bea33bcbbdd3c54c
eliashz <eliashz@gmail.com>
2022-05-24 15:06:36 +0200

    singletoon

--- Singleton/game.js
+++ Singleton/game.js
@@ -6,8 +6,10 @@ class Game {
     constructor (player1, player2) {
         this.player1 = player1;
         this.result1 = '';
+        this.score1 = 0;
         this.player2 = player2;
         this.result2 = '';
+        this.score2 = 0
     }
     
     play() {
@@ -18,16 +20,15 @@ class Game {
         if (this.result1 === this.result2) {
             console.log(`${this.result1} vs ${this.result2}: Draw!`); 
         } else if (((this.result1 === 'Rock') && (this.result2 === 'Scissors')) || ((this.result1 === 'Paper') && (this.result2 === 'Rock')) || ((this.result1 === 'Scissors') && (this.result2 === 'Paper'))){     
-            score1++
+            //score1++
+            this.score1 = this.score1 +1
             console.log(`${this.result1} vs ${this.result2}: ${this.player1} wins!`); 
-            console.log(score1);
+            console.log(this);
         } else {
-            score2++
-            console.log(score2);
+            this.score2 = this.score2 +1
+            console.log(this);
             console.log(`${this.result1} vs ${this.result2}: ${this.player2} wins!`);   
         }
-
-        console.log(new Singleton({total1: score1, total2: score2}));
 
  
     }
