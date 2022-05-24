8a817e8275ccd801a4ad0a0eb096f9c45964ca8c
eliashz <eliashz@gmail.com>
2022-05-24 15:11:39 +0200

    singleton ok

--- Singleton/app.js
+++ Singleton/app.js
@@ -16,13 +16,11 @@ game.play();
 game.play();
 
 const final = new Singleton({total1: game.score1, total2: game.score2});
-console.log(final.scoreboard);
 
-
-if (final.scoreboard.total1 > game3.score2) {
-    console.log(`The final result is: ${final.scoreboard.total1}. ${player1.name} from ${player1.team} wins!`);
-} else if (game3.score1 < game3.score2) {
-    console.log(`The final result is: ${final}. ${this.player2} wins!`);
+if (final.scoreboard.total1 > final.scoreboard.total2) {
+    console.log(`The final result is: ${final.scoreboard.total1} - ${final.scoreboard.total2}. ${player1.name} from ${player1.team} wins!`);
+} else if (final.scoreboard.total1 < final.scoreboard.total2) {
+    console.log(`The final result is: ${final.scoreboard.total1} - ${final.scoreboard.total2}. ${player2.name} from ${player2.team} wins!`);
 } else {
-    console.log(`The final result is: ${final.scoreboard}. Nobody wins. Let's play again!`);
-} */ 
+    console.log(`The final result is: ${final.scoreboard.total1} - ${final.scoreboard.total2}. Nobody wins. Let's play again!`);
+} 
--- Singleton/game.js
+++ Singleton/game.js
@@ -1,7 +1,3 @@
-let score1 = score2 = 0;
-
-const Singleton = require('./singleton');
-
 class Game {
     constructor (player1, player2) {
         this.player1 = player1;
@@ -16,17 +12,13 @@ class Game {
         const game = ['Rock', 'Paper', 'Scissors'];
         this.result1 = game[Math.floor(Math.random()*game.length)];
         this.result2 = game[Math.floor(Math.random()*game.length)];
-        //let score1 = score2 = 0;
         if (this.result1 === this.result2) {
             console.log(`${this.result1} vs ${this.result2}: Draw!`); 
         } else if (((this.result1 === 'Rock') && (this.result2 === 'Scissors')) || ((this.result1 === 'Paper') && (this.result2 === 'Rock')) || ((this.result1 === 'Scissors') && (this.result2 === 'Paper'))){     
-            //score1++
-            this.score1 = this.score1 +1
+            this.score1++;
             console.log(`${this.result1} vs ${this.result2}: ${this.player1} wins!`); 
-            console.log(this);
         } else {
-            this.score2 = this.score2 +1
-            console.log(this);
+            this.score2++;
             console.log(`${this.result1} vs ${this.result2}: ${this.player2} wins!`);   
         }
 
