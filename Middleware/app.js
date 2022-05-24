3a1e53ded0fc9df357439eb107f4c843921b79f0
eliashz <eliashz@gmail.com>
2022-05-24 17:29:11 +0200

    MW v2 wip

--- Middleware/app.js
+++ Middleware/app.js
@@ -9,24 +9,25 @@ const app = new Middleware(calculator);
 app.use((req, next) => {
   req.a = req.a ** 2;
   req.b = req.b ** 2;
-  console.log(`${req.a} y ${req.b}`);
+  console.log(`Middleware al cuadradrado: ${req.a} y ${req.b}`);
   next();
 });
 
 app.use((req, next) => {
   req.a= req.a ** 3;
-  req.b = req.b ** 3;
-  console.log(`${req.a} y ${req.b}`);
+  req.b= req.b ** 3;
+  console.log(`Middleware cubo: ${req.a} y ${req.b}`);
   next();
 });
 
 app.use((req, next) => {
   req.a= req.a / 2;
-  req.b = req.b / 2;
-  console.log(`${req.a} y ${req.b}`);
+  req.b= req.b / 2;
+
+  console.log(`Middleware división: ${req.a} y ${req.b}`);
   next();
 });
 
-//console.log(app.add(data[0]));
-//console.log(app.subtract(data[1]));
-console.log(app.multiply(data[0])); 
+console.log("Suma", app.add(data[0]));
+console.log("Resta", app.subtract(data[1]));
+console.log("Multiplicación", app.multiply(data[0])); 
