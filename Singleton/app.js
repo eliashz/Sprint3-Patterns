50bb80df9cc46a74878a0e08483c1fc1dcf063a8
eliashz <eliashz@gmail.com>
2022-05-24 15:06:29 +0200

    singleton mejor pinta

--- Singleton/app.js
+++ Singleton/app.js
@@ -7,29 +7,22 @@ const Singleton = require('./singleton');
 let player1 = new Player("Elías", "Red Team");
 let player2 = new Player("Berta", "Blue Team");
 
-let game1 = new Game (player1.name, player2.name);
-let game2 = new Game (player1.name, player2.name);
-let game3 = new Game (player1.name, player2.name);
-
-
+let game = new Game (player1.name, player2.name);
 
 console.log("****** Rock-paper-scissors Best of 3! ******");
 
+game.play();
+game.play();
+game.play();
+
+const final = new Singleton({total1: game.score1, total2: game.score2});
+console.log(final.scoreboard);
 
 
-game1.play();
-game2.play();
-game3.play();
-
-/* rockPaperScissors(game1);
-rockPaperScissors(game2);
-rockPaperScissors(game3); */
-
-
-/* if (game3.score1 > game3.score2) {
-    console.log(`The final result is: ${final.scoreboard}. ${this.player1} wins!`);
+if (final.scoreboard.total1 > game3.score2) {
+    console.log(`The final result is: ${final.scoreboard.total1}. ${player1.name} from ${player1.team} wins!`);
 } else if (game3.score1 < game3.score2) {
     console.log(`The final result is: ${final}. ${this.player2} wins!`);
 } else {
     console.log(`The final result is: ${final.scoreboard}. Nobody wins. Let's play again!`);
-} */
+} */ 
