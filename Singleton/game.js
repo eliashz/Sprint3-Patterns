5343705f4d77f372a0260815071ceba9850fc4d4
eliashz <eliashz@gmail.com>
2022-06-02 10:56:29 +0200

    singleton v2 ok?

--- Singleton/game.js
+++ Singleton/game.js
@@ -1,19 +1,17 @@
 const Singleton = require("./singleton");
-let draw = 0;
+
+let draw = score1 = score2 = 0;
+
 class Game {
-    constructor (player1, player2) {
+    constructor (player1, player2, final) {
         this.player1 = player1;
         this.result1 = '';
-        this.score1 = 0;
         this.player2 = player2;
         this.result2 = '';
-        this.score2 = 0
-        let final = new Singleton();
-  
+        this.final = new Singleton();
     }
-
+    
     play() {
-        
         const game = ['Rock', 'Paper', 'Scissors'];
         this.result1 = game[Math.floor(Math.random()*game.length)];
         this.result2 = game[Math.floor(Math.random()*game.length)];
@@ -21,15 +19,15 @@ class Game {
             console.log(`${this.result1} vs ${this.result2}: Draw!`); 
             draw++;
         } else if (((this.result1 === 'Rock') && (this.result2 === 'Scissors')) || ((this.result1 === 'Paper') && (this.result2 === 'Rock')) || ((this.result1 === 'Scissors') && (this.result2 === 'Paper'))){     
-            this.score1++;
+            score1++;
             console.log(`${this.result1} vs ${this.result2}: ${this.player1} wins!`); 
         } else {
-            this.score2++;
+            score2++;
             console.log(`${this.result1} vs ${this.result2}: ${this.player2} wins!`);   
         }
-        console.log(this.score1 + this.score2);
-        if (this.score1 + this.score2 + draw === 3) {
-            console.log("eehh", final({total1: this.score1, total2: this.score2}));
+        if (score1 + score2 + draw === 3) {
+            this.final.scoreboard = {total1: score1, total2: score2}; //Instanciar el Singleton al final de 3 partidas.
+            console.log("Final result: ", this.final);
         }
     }
 }
---
+++ Singleton/singleton_.js
@@ -0,0 +1,17 @@
+class Unica {
+    constructor(nombre) {
+        
+
+        if (Unica.instancia) {
+            console.log("creada");
+            return Unica.instancia;
+        } 
+        this.nombre = nombre;
+        Unica.instancia = this;
+        
+    }
+}
+
+const a = new Unica('a')
+const b = new Unica('b')
+console.log(a, b);
